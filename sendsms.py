d94970c9fd420aa1df97a90522adacd4f81cebcb
Kevin Dudeja <kevin.dudeja2015@vit.ac.in>
2017-03-23 19:56:30 +0530

    Code working with schedule

--- sendsms.py
+++ sendsms.py
@@ -1,14 +1,41 @@
 import twilio
+import schedule
+import time
 from twilio.rest import TwilioRestClient 
  
 # put your own credentials here 
 ACCOUNT_SID = "AC395e6e1f866844d26250ae25264d9c58" 
-AUTH_TOKEN = "Your_secret_auth_token" 
+AUTH_TOKEN = "6e124f1874d28954bbb2c47425acb37b" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
-client.messages.create(
-    to = "Your_number_with_country_code", 
-    from_="Your_twilio_outgoing_number", 
-    body ="Your_message", 
-)
+def send(t):
+	fo = open("out3.txt","r")
+	with fo as f:
+		content = f.readlines()
+
+	for i in content:
+		headline = i #content[0]
+		client.messages.create(
+			to = "+919790712004", 
+			from_="+13052406750", 
+			body = headline, 
+		)
+	
+	fs = open("out3.txt","w")
+	fs.write("")
+	fs.close()
+	
+	return
+
+print "Bot is running"
+
+#schedule.every().day.at("08:15").do(send,'')
+schedule.every().day.at("10:30").do(send,' ')
+
+while True:
+	schedule.run_pending()
+	time.sleep(5)
+
+
+
+
