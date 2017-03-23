ddb2d726a6e2d33699af3b5662600a33477b1d80
Kevin Dudeja <kevin.dudeja2015@vit.ac.in>
2017-03-23 19:55:02 +0530

    schedule not working, need to be fixed

--- inshorts.js
+++ inshorts.js
@@ -1,19 +1,39 @@
-console.log("Bot is working");
+console.log("Bot is Initialising...");
 
 var inshorts = require('inshorts').init();
+//var schedule = require('node-schedule');
 
-var category = ' ';
+//var j = schedule.scheduleJob('*/1 * * * *', get());
 
-inshorts.getNews(category,gotData);
-//inshorts.more({category:'startup',id:'jo8dm6fs-1'},error);
-function gotData(err,result){
-	var headlines = result.headline;
-	for(var i=0; i< headlines.length;i++){
-		console.log(headlines[i]);
-		
-	}
-	//if(!err)
-	//	console.log(result);
-	//else
-	//	console.log(err);
-};
+get();
+
+function get(){
+	console.log("Bot is working");
+
+	var category = ' ';
+	
+	var fs = require("fs");
+	fs.open('out3.txt', 'w+' , function(err,fd){
+		if(err){
+			return console.error(err);
+		}
+	});
+
+	inshorts.getNews(category,gotData);
+
+	//inshorts.more({category:'startup',id:'jo8dm6fs-1'},error);
+	function gotData(err,result){
+		var headlines = result.headline;
+		for(var i=0; i< headlines.length;i++){
+			//console.log(headlines[i]);
+			fs.appendFile('out3.txt', headlines[i]+'\r\n' , function(err){
+				if(err)
+					return console.error(err);
+			} )
+		}
+		//if(!err)
+		//	console.log(result);
+		//else
+		//	console.log(err);
+	};
+}
