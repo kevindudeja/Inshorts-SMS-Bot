007ece8e582d41a4b6ae942f0d21b5ad3d5d345b
Kevin Dudeja <kevin.dudeja2015@vit.ac.in>
2017-03-23 20:05:14 +0530

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -5,37 +5,32 @@ from twilio.rest import TwilioRestClient
  
 # put your own credentials here 
 ACCOUNT_SID = "AC395e6e1f866844d26250ae25264d9c58" 
-AUTH_TOKEN = "6e124f1874d28954bbb2c47425acb37b" 
+AUTH_TOKEN = "Your_secret_auth_token" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 def send(t):
-	fo = open("out3.txt","r")
+	fo = open("filename.txt","r")
 	with fo as f:
 		content = f.readlines()
 
 	for i in content:
 		headline = i #content[0]
 		client.messages.create(
-			to = "+919790712004", 
-			from_="+13052406750", 
+			to = "Your_number_with_country_code", 
+			from_="Your_twilio_outgoing_number", 
 			body = headline, 
 		)
-	
-	fs = open("out3.txt","w")
+	#open the file and clear it for the next day of headlines
+	fs = open("filename.txt","w")
 	fs.write("")
 	fs.close()
-	
-	return
+return
 
 print "Bot is running"
 
 #schedule.every().day.at("08:15").do(send,'')
-schedule.every().day.at("10:30").do(send,' ')
+schedule.every().day.at("10:30").do(send,' ') #schedule the function to run at 10:30AM
 
 while True:
 	schedule.run_pending()
 	time.sleep(5)
-
-
-
-
